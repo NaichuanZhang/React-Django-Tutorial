0bfc0382db47aaf29e95515e714129f8efa31306
alex <naichuan@umich.edu>
2018-03-22 19:04:55 -0400

    basic react

---
+++ js/main.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Shoppinglist from './shoppinglist'
+
+ReactDOM.render(
+  <Shoppinglist />,
+  document.getElementById('reactEntry'),
+);
